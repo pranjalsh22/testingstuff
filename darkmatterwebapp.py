275f57a1abd424e9d5861217b82332fa975fd894
Pranjal Sharma <physics0403@gmail.com>
2026-03-22 21:20:31 +0530

    Update darkmatterwebapp.py

--- darkmatterwebapp.py
+++ darkmatterwebapp.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
 import numpy as np
+altair==4.2.2
 #-----------------------------------------------------------------------------------------------------------
 #user defined functions
 def createdata(file):
